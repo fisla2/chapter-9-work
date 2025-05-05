7f5e9ae9486c5de770c357175afa602d9902def3
fisla2 <fisla1146362@mail.wcccd.edu>
2025-05-04 22:05:26 -0400

    Create ShowHorse.java

---
+++ ShowHorse.java
@@ -0,0 +1,12 @@
+// File: ShowHorse.java
+public class ShowHorse extends Horse {
+    private int pointsWon;
+
+    public int getPointsWon() {
+        return pointsWon;
+    }
+
+    public void setPointsWon(int pointsWon) {
+        this.pointsWon = pointsWon;
+    }
+}
