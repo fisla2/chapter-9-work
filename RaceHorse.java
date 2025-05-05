9f75f08a938c436ec8df2ac2f01b67af614c4a77
fisla2 <fisla1146362@mail.wcccd.edu>
2025-05-04 22:04:50 -0400

    Create RaceHorse.java

---
+++ RaceHorse.java
@@ -0,0 +1,13 @@
+// File: RaceHorse.java
+public class RaceHorse extends Horse {
+    private int racesWon;
+
+    public int getRacesWon() {
+        return racesWon;
+    }
+
+    public void setRacesWon(int racesWon) {
+        this.racesWon = racesWon;
+    }
+}
+
